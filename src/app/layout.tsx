cec6507e6b8da5a760a56934f97b18bbbba4fc14
hyunjung <sspp4555@naver.com>
2025-05-30 17:08:44 +0900

    update title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-// app/layout.tsx (또는 app/page.tsx 등 최상위 컴포넌트)
+// app/layout.tsx
 import { ReactNode } from 'react';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
@@ -6,6 +6,12 @@ import { AuthProvider } from './context/AuthContext';
 import './globals.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+
+export const metadata = {
+    title: '자기찾기연구소',
+    description: '청년들을 위한 출판사 자기찾기연구소에 오신 것을 환영합니다.',
+};
+
 export default async function RootLayout({ children }: { children: ReactNode }) {
     const supabase = createServerComponentClient({ cookies });
 

bd6bfb5c37382bd5df68f6466deb51e28c53c1e0
hyunjung <sspp4555@naver.com>
2025-06-04 01:04:37 +0900

    logo change

--- src/app/components/Smallhead.tsx
+++ src/app/components/Smallhead.tsx
@@ -6,5 +6,5 @@ type SmallheadProps = {
 export default function Smallhead({ title, color }: SmallheadProps) {
     const textColorClass = color === 'black' ? 'text-black' : 'text-white';
 
-    return <h2 className={`waguri-font ${textColorClass} text-4xl md:text-6xl mb-4 tracking-tight`}>{title}</h2>;
+    return <h2 className={`waguri-font ${textColorClass} text-4xl md:text-5xl mb-4 tracking-tight`}>{title}</h2>;
 }
